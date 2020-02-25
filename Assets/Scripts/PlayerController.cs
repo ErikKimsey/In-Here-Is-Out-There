cfcc5f3d7e381b9ff431fe10410a78b2b0cede6b
Erik Kimsey <kimseyerik@gmail.com>
2020-02-25 16:44:20 -0500

    Added r-controller

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public FixedJoystick joystick;
     void Start()
     {
         
