2e1d04a3108154a686085efe4f23c7aaec64875c
Erik Kimsey <kimseyerik@gmail.com>
2020-02-27 17:56:11 -0500

    New rotation logic.  But, still janky

--- Assets/Scripts/ThirdPersonPlayerController.cs
+++ Assets/Scripts/ThirdPersonPlayerController.cs
@@ -9,16 +9,30 @@ public class ThirdPersonPlayerController : MonoBehaviour
     private float inputX, inputY;
     public DynamicJoystick joystick;
     public Transform rotationTarget;
+    public Vector3 anglesToRotate;
+    private Vector3 currentDirectionVertex;
+    public Rigidbody rb;
     void Start()
     {
-        
+      rb = GetComponent<Rigidbody>();
+      currentDirectionVertex = Vector3.forward * Time.deltaTime;
     }
 
-    // Update is called once per frame
+    
     void FixedUpdate()
     {
         HandleZMovement();
-        
+        NewRotate();      
+    }
+
+    void NewRotate(){
+        anglesToRotate = new Vector3(0, joystick.Horizontal * rotationSpeed * Time.deltaTime, 0);
+        transform.Rotate(anglesToRotate);
+    }
+
+    void HandleRBRotattion(Vector3 rot){
+      
+      rb.transform.Rotate(rot);
     }
 
     void HandleZMovement()
@@ -28,39 +42,34 @@ public class ThirdPersonPlayerController : MonoBehaviour
         inputX = joystick.Horizontal * moveSpeed * Time.deltaTime;
         if (inputY > 0)
         {
+            currentDirectionVertex = Vector3.forward * Time.deltaTime;
             transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
             HandleXMovement();
-            //transform.Rotate(Vector3.up, inputY * -rotationSpeed * Time.deltaTime);
         }
         if (inputY < 0)
         {
+            currentDirectionVertex = -Vector3.forward * Time.deltaTime;
             transform.Translate(-Vector3.forward * moveSpeed * Time.deltaTime);
             HandleXMovement();
-            //transform.Rotate(Vector3.up, inputY * rotationSpeed * Time.deltaTime);
-            //HandleRotation(inputX);
         }
-    }
-
-    void HandleXMovement()
-    {
-        Debug.Log(joystick.Horizontal);
-        inputX = joystick.Horizontal * moveSpeed * Time.deltaTime;
-        if (inputX > 0)
-        {
-            transform.Translate(Vector3.right * moveSpeed * Time.deltaTime);
-            //transform.Rotate(Vector3.up, inputY * -rotationSpeed * Time.deltaTime);
-        }
-        if (inputX < 0)
-        {
-            transform.Translate(Vector3.left * moveSpeed * Time.deltaTime);
-            //transform.Rotate(Vector3.up, inputY * rotationSpeed * Time.deltaTime);
+        if(inputY == 0){
+          transform.Translate(currentDirectionVertex);
         }
         
     }
 
-    void HandleRotation(float xRot)
+    void HandleXMovement()
     {
-        transform.Rotate(Vector3.up, xRot * rotationSpeed * Time.deltaTime);
+        inputX = joystick.Horizontal * moveSpeed * Time.deltaTime;
+        if (inputX > 0)
+        {
+            transform.Translate(Vector3.right * moveSpeed * Time.deltaTime);
+            HandleRBRotattion(Vector3.right * moveSpeed * Time.deltaTime * 2);
+        }
+        if (inputX < 0)
+        {
+            transform.Translate(Vector3.left * moveSpeed * Time.deltaTime);
+            HandleRBRotattion(Vector3.left * moveSpeed * Time.deltaTime * 2);
+        } 
     }
-
 }
