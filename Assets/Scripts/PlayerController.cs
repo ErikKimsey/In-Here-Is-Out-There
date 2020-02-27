a3d315964caa034831133b7bb9887d7bb5a4e524
Erik Kimsey <kimseyerik@gmail.com>
2020-02-27 10:26:19 -0500

    Additional player controllers"

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public FixedJoystick joystick;
+    
+
+
+    /**
+
+            
+    */
+    public float speed;
+    public DynamicJoystick joystick;
+    public Rigidbody rb;
     void Start()
     {
         
     }
 
     // Update is called once per frame
-    void Update()
+    public void FixedUpdate()
     {
-        
+        Vector3 direction = Vector3.forward * joystick.Vertical + Vector3.right * joystick.Horizontal;
+        rb.AddForce(direction * speed * Time.fixedDeltaTime, ForceMode.VelocityChange);
+        Debug.Log(joystick.Vertical);
+        transform.Rotate(-joystick.Vertical / 10, 0f, -joystick.Horizontal/10);
     }
 }
